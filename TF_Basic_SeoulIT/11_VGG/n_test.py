8b478919c4b8862a7b3128f6ed42a938b6f11f2a
pythonstart <60255225+pythonstart@users.noreply.github.com>
2020-06-03 17:00:30 +0900

    20200603_update
    20200603_update

---
+++ TF_Basic_SeoulIT/11_VGG/n_test.py
@@ -0,0 +1,7 @@
+import  tensorflow as tf
+from    tensorflow import  keras
+from    tensorflow.keras import datasets, layers, optimizers, models
+from    tensorflow.keras import regularizers
+
+class VGG11(models.Model):
+	pass
